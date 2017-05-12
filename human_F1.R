803c245d5ebb4dde70f3e6516b11fe835a6f18b3
Chen, Lei <LChen@ETS.ORG>
2017-05-12 10:05:29 -0400

    changed tSNE vet's corresponding label metadata

--- human_F1.R
+++ human_F1.R
@@ -1,7 +1,19 @@
 # 
 # On the test set, output final and individual raters' rating scors.
-# score need be factorized to <=4, ==5, and >=6
 # then, will use sklearn classification report to estimate human F-1
+library(tidyverse)
+library(magrittr)
 
-hs_tidy_test <- merge(amt_hs, resp_test_lst, by="vid")
-write.csv(hs_tidy_test, file='tSNE/hs_test.csv', row.names = F)
+merge_ <- function(x, y){
+  out <- merge(x, y, by='vid') %>%
+    select(vid:final)
+  out
+  
+}
+
+write.csv(merge_(amt_hs_asr, resp_test_lst), file='tSNE/hs_test.csv', row.names = F)
+write.csv(merge_(amt_ag_asr, resp_test_lst), file='tSNE/ag_test.csv', row.names = F)
+write.csv(merge_(amt_ex_asr, resp_test_lst), file='tSNE/ex_test.csv', row.names = F)
+write.csv(merge_(amt_co_asr, resp_test_lst), file='tSNE/co_test.csv', row.names = F)
+write.csv(merge_(amt_em_asr, resp_test_lst), file='tSNE/em_test.csv', row.names = F)
+write.csv(merge_(amt_op_asr, resp_test_lst), file='tSNE/op_test.csv', row.names = F)
